162f0d68a0497fba7882a7aa56b2f922b0b3dc75
Samirramirodiaz <samir.r.diaz11@gmail.com>
2021-09-27 16:21:07 -0300

    add vector icons, icon eye into login and functionality into login

--- FrontendMobile/screen/LoginScreen.jsx
+++ FrontendMobile/screen/LoginScreen.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View } from 'react-native';
 import { CustomButton } from '../components/UI/CustomButton';
 import TouchableComponent from '../components/UI/TouchableComponent';
 import { Colors } from '../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ navigation }) => {
+
+    const [userData, setUserData] = useState({ password: '', username: '' });
+    const [hidePassword, setHidePassword] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleUserChange = (name, value) => {
+        setUserData({
+            ...userData,
+            [name]: value
+        })
+    }
+
+    const login = () => {
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2300);
+    }
+
+    const navigateTo = (nameScreen) => navigation.navigate(nameScreen);
 
     return (
-        <KeyboardAvoidingView style={styles.screen}>
+        <KeyboardAvoidingView style={styles.screen} behavior={Platform.OS === 'ios' ? 'padding' : null}>
             <View style={styles.container}>
                 <View style={styles.imageRow}>
                     <Image
@@ -23,6 +44,8 @@ export const LoginScreen = () => {
                                 placeholder='Usuario o télefono'
                                 style={styles.input}
                                 autoCapitalize='none'
+                                value={userData.username}
+                                onChangeText={(value) => handleUserChange('username', value)}
                             />
                         </View>
                         <View style={styles.inputRow}>
@@ -30,11 +53,21 @@ export const LoginScreen = () => {
                                 placeholder='Contraseña'
                                 style={styles.input}
                                 autoCapitalize='none'
+                                value={userData.password}
+                                secureTextEntry={hidePassword}
+                                onChangeText={(value) => handleUserChange('password', value)}
+                            />
+                            <Ionicons
+                                name={hidePassword ? 'eye-off' : 'eye'}
+                                size={22}
+                                color={hidePassword ? Colors.dark : Colors.secondary}
+                                style={styles.eyeIcon}
+                                onPress={() => setHidePassword(!hidePassword)}
                             />
                         </View>
                     </View>
                     <View style={styles.forgotButton}>
-                        <TouchableComponent onPress={() => console.log('si me re olvidé man')}>
+                        <TouchableComponent onPress={() => navigateTo('ForgotPassword')}>
                             <View>
                                 <Text style={styles.forgotButtonText}>¿Olvidaste tu contraseña?</Text>
                             </View>
@@ -44,12 +77,12 @@ export const LoginScreen = () => {
                 <View style={styles.loginButtonRow}>
                     <CustomButton
                         disabled={false}
-                        onPress={() => console.log('awd')}
+                        onPress={login}
                         title='Iniciar sesión'
-                        isLoading={false}
+                        isLoading={isLoading}
                     />
                     <View style={styles.registerRow}>
-                        <TouchableComponent onPress={() => console.log('no tengo brou')}>
+                        <TouchableComponent onPress={() => navigateTo('Register')}>
                             <View style={styles.registerButton}>
                                 <Text style={styles.registerButtonText}>¿No tenés una cuenta?
                                     <Text style={{ ...styles.registerButtonText, fontFamily: 'montserrat-semi-bold', color: Colors.primary }}> Registrate.</Text>
@@ -85,19 +118,27 @@ const styles = StyleSheet.create({
     },
     form: {
         flex: 3,
+        width: '100%',
     },
     formSection: {
         flex: 1,
     },
     inputRow: {
         flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     input: {
         fontSize: 18,
+        width: '100%',
         fontFamily: 'montserrat-medium',
         borderBottomColor: 'black',
         borderBottomWidth: 0.5,
-        paddingVertical: 5
+        paddingVertical: 5,
+    },
+    eyeIcon: {
+        padding: 10,
+        marginLeft: 'auto',
     },
     forgotButton: {
         flex: 1,
@@ -122,6 +163,6 @@ const styles = StyleSheet.create({
     registerButtonText: {
         fontFamily: 'montserrat-medium',
         color: Colors.dark,
-        fontSize: 16,
+        fontSize: 14,
     }
 })
